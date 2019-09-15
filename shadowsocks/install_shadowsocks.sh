d705010b3c0fcded76134590e14df297ff509a73
wangzhihao <wangzh@getui.com>
2019-09-15 22:16:04 +0800

    enable shadowsocks service

--- shadowsocks/install_shadowsocks.sh
+++ shadowsocks/install_shadowsocks.sh
@@ -4,4 +4,11 @@ yum update
 yum install python-setuptools && easy_install pip
 pip install shadowsocks
 
+# modify ssserver port and password in shadowsocks.json
+cp shadowsocks.json /etc/shadowsocks.json
+
+cp shadowsocks.service /usr/lib/systemd/system/
+
+systemctl enable shadowsocks
+systemctl start shadowsocks
 
