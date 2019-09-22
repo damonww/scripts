6b5b4df4942b0aa81e607fca3426f4826ee52e6b
damonww <damonww@protonmail.com>
2019-09-22 22:03:11 +0800

    add comment for changing mysql password

--- install_mysql_on_centos.sh
+++ install_mysql_on_centos.sh
@@ -8,3 +8,5 @@ yum install mysql-community-server -y
 service mysqld start
 
 grep 'temporary password' /var/log/mysqld.log >> passwd.log
+
+# then change password: SET PASSWORD = PASSWORD('your_new_password');
