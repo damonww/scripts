ec2b9c1049cfb29e7b71de0615ed875f41ec7344
wangzhihao <wangzh@getui.com>
2020-04-27 22:00:00 +0800

    let's encrypt

--- let_us_encrypt.sh
+++ let_us_encrypt.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+subscription-manager repos --enable "rhel-*-optional-rpms" --enable "rhel-*-extras-rpms"  --enable "rhel-ha-for-rhel-*-server-rpms"
 yum -y install yum-utils
 yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional
 yum install certbot python2-certbot-nginx
